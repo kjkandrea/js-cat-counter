bd397a38b6344360347382403f7f8c9f56e98b7b
jongkeun-kim <kjkandrea@gmail.com>
2021-12-11 15:20:27 +0900

    Add emit structure

--- src/views/Counter.js
+++ src/views/Counter.js
@@ -1,29 +1,28 @@
 class Counter {
   init (el, count) {
     this._el = el
+    this._events = {
+      '@count:increment': [() => console.log('increment')],
+      '@count:decrement': [() => console.log('decrement')],
+    }
 
     this.setCount(count)
     this.attachEvents()
   }
 
-  setCount(count) {
+  setCount (count) {
     this._el.children.counter.value = count
   }
 
   attachEvents () {
     const increment = this._el.children.increment
     const decrement = this._el.children.decrement
-    const counter = this._el.children.counter
-    increment.addEventListener('click', () => this.increment(counter))
-    decrement.addEventListener('click', () => this.decrement(counter))
+    increment.addEventListener('click', () => this.emit('@count:increment'))
+    decrement.addEventListener('click', () => this.emit('@count:decrement'))
   }
 
-  increment(inputEl) {
-    inputEl.value = Number(inputEl.value) + 1
-  }
-
-  decrement(inputEl) {
-    if (inputEl.value >= 1) inputEl.value = Number(inputEl.value) - 1
+  emit (eventName) {
+    this._events[eventName].forEach(fn => fn())
   }
 }
 
