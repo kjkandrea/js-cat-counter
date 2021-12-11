62279070a612ddd3cec10086bc1ea2e248ef5177
jongkeun-kim <kjkandrea@gmail.com>
2021-12-11 14:46:36 +0900

    Add attach event +/-

--- src/views/Counter.js
+++ src/views/Counter.js
@@ -1,8 +1,23 @@
 class Counter {
-  init(el) {
+  init (el) {
     this._el = el
 
+    this.attachEvents()
     console.log(this._el)
+  }
+
+  attachEvents () {
+    const [incrementEl, decrementEl] = this._el.querySelectorAll('button')
+    incrementEl.addEventListener('click', () => this.increment())
+    decrementEl.addEventListener('click', () => this.decrement())
+  }
+
+  increment() {
+    console.log('+')
+  }
+
+  decrement() {
+    console.log('-')
   }
 }
 
