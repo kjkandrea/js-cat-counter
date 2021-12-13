5bac95ce8412010dcc2f1fbfc2c0de51d584cda5
jongkeun-kim <kjkandrea@gmail.com>
2021-12-13 23:02:00 +0900

    Add counter container

---
+++ src/components/CounterContainer.svelte
@@ -0,0 +1,14 @@
+<script>
+    import CatList from './presentations/CatList.svelte'
+    import Counter from './presentations/Counter.svelte'
+
+    let count = 0;
+
+    const increment = () => count++
+    const decrement = () => count >= 1 && count--
+</script>
+
+<div>
+    <Counter count={count} increment={increment} decrement={decrement} />
+    <CatList count={count} />
+</div>
--- src/components/presentations/Counter.svelte
+++ src/components/presentations/Counter.svelte
@@ -1,15 +1,13 @@
 <script>
     import CatList from './CatList.svelte'
 
-    let count = 0
-
-    const increment = () => count++
-    const decrement = () => count >= 1 && count--
+    export let count;
+    export let increment
+    export let decrement
 </script>
 
 <div>
     <button on:click={increment}>+</button>
     <input type="number" readonly value={count}/>
     <button on:click={decrement}>_</button>
-    <CatList count={count} />
 </div>
